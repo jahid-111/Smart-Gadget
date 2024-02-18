32f357da2d670adec62971a1f29c11f00856fb23
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-18 05:32:01 +0300

    linkToHeadphone

--- javaScript/main.js
+++ javaScript/main.js
@@ -7,6 +7,7 @@
 
 
 const productDefaultsDisplay = {
+    displayName : "Popular Products",
     items: [
         {
             brand: "Apple",
@@ -14,7 +15,7 @@ const productDefaultsDisplay = {
             image : 'Images/loadJs/testimg.jpg'
         },
         {
-            brand: "Apple",
+            brand: "Honor",
             name: "iPad",
             image : 'Images/loadJs/testimg.jpg'
         },
@@ -55,21 +56,34 @@ const productDefaultsDisplay = {
 };
 
 const productStore =  productDefaultsDisplay.items;
+const displayNone =  document.getElementById('HeadphoneDisplay');
 
+    const nameOfDisplay = document.getElementById('nameOfDisplay')
+    nameOfDisplay.innerText= productDefaultsDisplay.displayName;
+
+
+    // ----- 
     const productDisplays = document.getElementById('productDisplay')
-    productStore.forEach(display => {
-        const columnDiv =  document.createElement('div');
-        columnDiv.innerHTML = ` 
-                        <div class="card card-color">
-                                <img class="p-1 shadow-lg " src="${display.image}" class="card-img-top" alt="...">
-                             <div class="card-body">
-                                 <h5 class="card-title">Product Catagory</h5>
-                                 <p class="card-text"> (Write 2 lines about this product )This is a longer card with supporting text below.</p>
-                             </div>
-                         </div>
-            `;
-        // console.log(columnDiv)
-        productDisplays.appendChild(columnDiv)
+        productStore.forEach(display => {
+            const columnDiv =  document.createElement('div');
+              columnDiv.innerHTML = ` 
+                            <div class="card card-color">
+                                    <img class="p-1 shadow-lg " src="${display.image}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">${display.brand} </h5>
+                                    <p class="card-text"> (Write 2 lines about this product )This is a longer card with supporting text below.</p>
+                                </div>
+                            </div>
+                `;
+    productDisplays.appendChild(columnDiv)
 
+        });
 
-    });
+       
+
+            
+//         const displaySection = document.getElementById('displaySection');
+//         const hdphnSection = document.getElementById('hdphnSection');
+
+// console.log(displaySection, hdphnSection);
+
